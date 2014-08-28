87d7bf2e3be2822b28a8397631ebff1a0cf78366
Matthew Goodall <dogsaw@thecyberplains.com>
2014-08-28 14:04:17 +0100

    Add versioning to the install process
    This came about because I had an earlier version of the registry key
    installed, and the installer wouldn't create the correct one. That of
    course, caused the driver to not load. There is now a new "Version"
    value in the registry that the install script can check against. It is
    directly on the kblayout key, indicating that I might want to move
    "Selected Layout" there.

--- src/installer/install.ps1
+++ src/installer/install.ps1
@@ -11,6 +11,7 @@
 
 $DRIVER_NAME = "kblayout"
 $DRIVER_PATH = "$($ENV:WinDir)\System32\Drivers\$DRIVER_NAME.sys"
+$REGISTRY_BASE = "HKLM:\SYSTEM\CurrentControlSet\services\$DRIVER_NAME"
 
 ##
 # Check if the user has correct permissions to install the driver
@@ -37,29 +38,41 @@ if ($arch -eq "64-bit"){
 }
 
 ##
-# Set up the registry entries
+# Register and configure the driver
 ##
-Push-Location
-Set-Location HKLM:
 
-# Register the driver
-if (-not (Test-Path ".\SYSTEM\CurrentControlSet\services\$DRIVER_NAME") ) {
+# Check the version
+$version = "0.0.0"
+if (Test-Path $REGISTRY_BASE ) {
+    $regKey = Get-Item $REGISTRY_BASE
+    if ($regKey.GetValue("Version")) {
+        $version = $regKey.GetValue("Version")
+    }
+
+}
+
+if ($version -lt "1.0.0") {
+    # Install the driver
+
     echo "Registering driver"
 
     # using the full path for the ImagePath property causes the driver to not load properly
     # however, a path relative to %winddir% works fine
     $imagePath = $DRIVER_PATH.Substring($Env:WinDir.Length + 1) # includes the / as well
 
-    $kblayoutService = New-Item .\SYSTEM\CurrentControlSet\services -Name $DRIVER_NAME
+    $kblayoutService = New-Item "HKLM:\SYSTEM\CurrentControlSet\services" -Name $DRIVER_NAME
     $kblayoutService | New-ItemProperty -Name ImagePath -Type ExpandString -Value $imagePath
     $kblayoutService | New-ItemProperty -Name Group -Type String -Value "Keyboard Class"
     $kblayoutService | New-ItemProperty -Name ErrorControl -Type Dword -Value 1
     $kblayoutService | New-ItemProperty -Name Start -Type Dword -Value 3
     $kblayoutService | New-ItemProperty -Name Type -Type Dword -Value 1
+
+    $version = "1.0.0"
+    $kblayoutService | New-ItemProperty -Name Version -Type String -Value "1.0.0"
 }
 
 # Add the driver to the keyboard filters
-$kbdClass = Get-Item ".\SYSTEM\CurrentControlSet\Control\Class\{4D36E96B-E325-11CE-BFC1-08002BE10318}"
+$kbdClass = Get-Item "HKLM:\SYSTEM\CurrentControlSet\Control\Class\{4D36E96B-E325-11CE-BFC1-08002BE10318}"
 $upperFilters = ($kbdClass | Get-ItemProperty -Name UpperFilters).UpperFilters
 if (-not ($upperFilters -contains $DRIVER_NAME)) {
     Write-Host "Adding to keyboard filters"
@@ -67,8 +80,6 @@ if (-not ($upperFilters -contains $DRIVER_NAME)) {
     $newFilters = $upperFilters + $DRIVER_NAME
     $kbdClass | Set-ItemProperty -Name UpperFilters -Value $newFilters
 }
-
-Pop-Location
 
 Write-Host "Installation complete"
 Write-Host "Press any key to continue..."
